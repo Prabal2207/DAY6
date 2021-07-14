272d11dad8914844aa7a9f71a3d093f1c64f4c80
Prabal2207 <prabal760@gmail.com>
2021-07-14 07:59:17 +0530

    [Prabal] Added:- Vending machine for less number of denomination is implemented .

---
+++ NUnit_Testing/VendingMachine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+
+        public static void VendingMachine(int Rupees)
+        {
+            int[] notes = new int[] { 1000, 500, 200, 100, 50, 20, 10, 5, 1 };
+            int[] Count = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (Rupees >= notes[i])
+                {
+                    Count[i] = Rupees / notes[i];
+                    Rupees = Rupees - Count[i] * notes[i];
+                }
+            }
+
+           
+            Console.WriteLine("Currency Count ->");
+            for (int i = 0; i < 9; i++)
+            {
+                if (Count[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : "
+                        + Count[i]);
+                }
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Enter amount");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            VendingMachine(amount);
+        }
+    }
+}
