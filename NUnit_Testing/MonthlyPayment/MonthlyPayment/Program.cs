c6a48069856a4c12e0c0d440749b9707d0ff8037
Prabal2207 <prabal760@gmail.com>
2021-07-14 08:04:05 +0530

    [Prabal] Added:- Monthly Payment is calculated using formula.

---
+++ NUnit_Testing/MonthlyPayment/MonthlyPayment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MonthlyPayment
+{
+    class monthlyPayment
+    {
+        public static void Main()
+        {
+            monthlyPayments();
+        }
+        public static void monthlyPayments()
+        {
+            Console.Write("Enter years : ");
+
+                int Y = int.Parse(Console.ReadLine());
+            Console.Write("Enter principal loan amount: ");
+            double P = double.Parse(Console.ReadLine());
+            Console.Write("Enter percent interest compounded monthly: ");
+            double R = double.Parse(Console.ReadLine());
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double payment = (P * r) / (1 - Math.Pow((1 + r), (-n)));
+
+            Console.WriteLine("Your Payment is: " + payment);
+        }
+    }
+}
