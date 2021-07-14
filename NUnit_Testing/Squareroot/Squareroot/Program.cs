81a941fb982715c1c97c726dc83024138aa0aa5c
Prabal2207 <prabal760@gmail.com>
2021-07-14 08:04:55 +0530

    [Prabal] Added:- Square root of non negative number is calculated.

---
+++ NUnit_Testing/Squareroot/Squareroot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Squareroot
+{
+    class Squareroot
+    {
+        public static void Main()
+        {
+            sqrt();
+        }
+
+        public static void sqrt()
+        {
+            Console.Write("Enter number: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double t = c;
+
+            double epsilon = 1e-15;
+
+
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = ((c / t) + t) / 2;
+
+            }
+
+            Console.WriteLine("Square Root of {0} is {1}", c, t);
+        }
+
+
+    }
+}
