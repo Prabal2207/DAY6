d1ded679d84b17d5ef06e3bbc133c06d7f13edb8
Prabal2207 <prabal760@gmail.com>
2021-07-14 07:50:06 +0530

    [Prabal] Added:- Fibonacci Series is implemented

---
+++ LogicalPrograms/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci Series Computation");
+            Fib();
+        }
+
+        public static void Fib()
+        {
+            Console.WriteLine("Enter range ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int result=0;
+            int i = 0;
+
+            while (i < N)
+            {
+                result = a + b;
+                b = result;
+                i++;
+
+            }
+            Console.WriteLine(value: "Fibonacci : " + result);
+        }
+    }
+}
