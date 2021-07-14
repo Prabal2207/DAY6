df89fb4b788222c3efa463ae058d9ede2e1de960
Prabal2207 <prabal760@gmail.com>
2021-07-14 07:52:20 +0530

    [Prabal] Added:- Perfect number implementation.

---
+++ LogicalPrograms/PerfectNumber/PerfectNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perfect Number Implementation:");
+            PerfectNumber();
+        }
+
+        public static void PerfectNumber()
+        {
+            Console.WriteLine("Enter Number to be checked for perfect number");
+            int num= Convert.ToInt32(Console.ReadLine());
+            
+            int sum = 0;
+
+            for(int i=1;i<=num/2;i++)
+            {
+                if(num%i == 0)
+                { 
+                    sum += i;
+                }
+                
+            }
+            
+            if (sum == num)
+            {
+                Console.WriteLine(num + " is perfect number.");
+            }
+            else
+            {
+                Console.WriteLine(num +" is not a perfect number.");
+            }
+
+            
+        }
+    }
+}
